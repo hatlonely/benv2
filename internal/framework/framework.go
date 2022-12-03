2884d7da6562f87043bc189728ec148d71ae8b19
hatlonely <hatlonely@gmail.com>
2022-12-03 21:03:45 +0800

    update

---
+++ internal/framework/framework.go
@@ -0,0 +1,184 @@
+package framework
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/hatlonely/go-kit/refx"
+
+	"github.com/hatlonely/benv2/internal/driver"
+	"github.com/hatlonely/benv2/internal/eval"
+	"github.com/hatlonely/benv2/internal/source"
+	"github.com/pkg/errors"
+)
+
+type Options struct {
+	Name   string
+	Ctx    map[string]refx.TypeOptions
+	Source map[string]refx.TypeOptions
+	Plan   struct {
+		Duration time.Duration
+		Unit     []struct {
+			Parallel int `dft:"1"`
+			Name     string
+			Step     []*struct {
+				Ctx string
+				Req interface{}
+			}
+		}
+	}
+}
+
+func NewFrameworkWithOptions(options *Options, opts ...refx.Option) (*Framework, error) {
+	var err error
+	fw := &Framework{
+		name:   options.Name,
+		ctx:    map[string]driver.Driver{},
+		source: map[string]source.Source{},
+	}
+
+	for key, refxOptions := range options.Ctx {
+		fw.ctx[key], err = driver.NewDriverWithOptions(&refxOptions, opts...)
+		if err != nil {
+			return nil, errors.WithMessage(err, "driver.NewDriverWithOptions failed")
+		}
+	}
+
+	for key, refxOptions := range options.Source {
+		fw.source[key], err = source.NewSourceWithOptions(&refxOptions, opts...)
+		if err != nil {
+			return nil, errors.WithMessage(err, "source.NewSourceWithOptions failed")
+		}
+	}
+
+	var plan PlanInfo
+	for _, unitDesc := range options.Plan.Unit {
+		var step []*StepInfo
+		for _, stepDesc := range unitDesc.Step {
+			e, err := eval.NewEvaluable(stepDesc.Req)
+			if err != nil {
+				return nil, errors.WithMessage(err, "eval.NewEvaluable failed")
+			}
+			step = append(step, &StepInfo{
+				Ctx: stepDesc.Ctx,
+				Req: e,
+			})
+		}
+		plan.Unit = append(plan.Unit, &UnitInfo{
+			Parallel: unitDesc.Parallel,
+			Name:     unitDesc.Name,
+			Step:     step,
+		})
+	}
+
+	fw.plan = &plan
+
+	return fw, nil
+}
+
+type Framework struct {
+	name   string
+	ctx    map[string]driver.Driver
+	source map[string]source.Source
+	plan   *PlanInfo
+}
+
+type PlanInfo struct {
+	Duration time.Duration
+	Unit     []*UnitInfo
+}
+
+type UnitInfo struct {
+	Parallel int
+	Name     string
+	Step     []*StepInfo
+}
+
+type StepInfo struct {
+	Ctx string
+	Req *eval.Evaluable
+}
+
+type UnitStat struct {
+	Name string
+	Res  []interface{}
+	Err  []error
+}
+
+func (fw *Framework) Stat(stat *UnitStat) {
+
+}
+
+func (fw *Framework) RunPlan(info *PlanInfo) error {
+	var wg sync.WaitGroup
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	for _, unit := range info.Unit {
+		for i := 0; i < unit.Parallel; i++ {
+			wg.Add(1)
+			go func(unit *UnitInfo) {
+			out:
+				for {
+					select {
+					case <-ctx.Done():
+						break out
+					case <-time.After(info.Duration):
+						break out
+					default:
+						stat, err := fw.RunUnit(unit)
+						if err != nil {
+							cancel()
+						}
+						fw.Stat(stat)
+					}
+				}
+
+				wg.Done()
+			}(unit)
+		}
+	}
+
+	return nil
+}
+
+func (fw *Framework) RunUnit(info *UnitInfo) (*UnitStat, error) {
+	stat := &UnitStat{Name: info.Name}
+	var err error
+
+	// fetch source
+	sourceMap := map[string]interface{}{}
+	for key, src := range fw.source {
+		sourceMap[key] = src.Fetch()
+	}
+	for _, step := range info.Step {
+		var req interface{}
+		req, err = step.Req.Evaluate(map[string]interface{}{
+			"source": sourceMap,
+			"stat":   stat,
+		})
+		if err != nil {
+			return nil, errors.WithMessage(err, "step.Req.Evaluate failed")
+		}
+		d, ok := fw.ctx[step.Ctx]
+		if !ok {
+			return nil, errors.Errorf("ctx not found. ctx: [%s]", step.Ctx)
+		}
+		res, err := d.Do(req)
+		if err != nil {
+			err = errors.WithMessage(err, "driver.Do failed")
+			break
+		}
+
+		stat.Res = append(stat.Res, res)
+		stat.Err = append(stat.Err, nil)
+	}
+
+	if err != nil {
+		stat.Res = append(stat.Res, nil)
+		stat.Err = append(stat.Err, err)
+	}
+
+	return stat, nil
+}
