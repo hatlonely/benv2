3a86e03224e2ec3aea55436c2c15b5645de0dac7
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2022-12-06 01:06:59 +0800

    update

---
+++ internal/recorder/recorder_file_test.go
@@ -0,0 +1,97 @@
+package recorder
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hatlonely/go-kit/refx"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFileRecorder(t *testing.T) {
+	Convey("TestFileRecorder", t, func() {
+		fr, err := NewFileRecorderWithOptions(&FileRecorderOptions{
+			FilePath: "ben.json",
+		})
+		So(err, ShouldBeNil)
+
+		Convey("Record", func() {
+			So(fr.Record(&UnitStat{
+				Name:    "test-name",
+				Step:    []*StepStat{
+					{
+						Time:    "2022-12-05T16:36:53.653687+08:00",
+						Req:     map[string]interface{}{
+							"Key1": "val1",
+							"Key2": "val2",
+						},
+						Res:     map[string]interface{}{
+							"Key3": "val3",
+							"Key4": "val4",
+						},
+						Err:     nil,
+						ErrCode: "",
+						ResTime: 2 * time.Millisecond,
+					},
+				},
+				ErrCode: "",
+				ResTime: 2 * time.Millisecond,
+			}), ShouldBeNil)
+
+			So(fr.Record(&UnitStat{
+				Name:    "test-name",
+				Step:    []*StepStat{
+					{
+						Time:    "2022-12-05T16:36:54.653687+08:00",
+						Req:     map[string]interface{}{
+							"Key1": "val1",
+							"Key2": "val2",
+						},
+						Res:     map[string]interface{}{
+							"Key3": "val3",
+							"Key4": "val4",
+						},
+						Err:     nil,
+						ErrCode: "",
+						ResTime: 2 * time.Millisecond,
+					},
+				},
+				ErrCode: "",
+				ResTime: 2 * time.Millisecond,
+			}), ShouldBeNil)
+
+			So(fr.Record(&UnitStat{
+				Name:    "test-name",
+				Step:    []*StepStat{
+					{
+						Time:    "2022-12-05T16:36:55.653687+08:00",
+						Req:     map[string]interface{}{
+							"Key1": "val1",
+							"Key2": "val2",
+						},
+						Res:     map[string]interface{}{
+							"Key3": "val3",
+							"Key4": "val4",
+						},
+						Err:     nil,
+						ErrCode: "",
+						ResTime: 2 * time.Millisecond,
+					},
+				},
+				ErrCode: "",
+				ResTime: 2 * time.Millisecond,
+			}), ShouldBeNil)
+
+			So(fr.Close(), ShouldBeNil)
+
+			Convey("TimeRange", func() {
+				st, et, err := fr.TimeRange()
+				refx.Must(err)
+				fmt.Println(st, et, err)
+			})
+		})
+
+
+	})
+}
