e23f256d03c64948fe1bdf706529d91a9511f4cc
hatlonely <hatlonely@gmail.com>
2022-12-06 01:28:02 +0800

    update

---
+++ internal/recorder/analyst.go
@@ -0,0 +1,35 @@
+package recorder
+
+import (
+	"reflect"
+	"time"
+
+	"github.com/hatlonely/go-kit/refx"
+	"github.com/pkg/errors"
+)
+
+func RegisterAnalyst(key string, constructor interface{}) {
+	refx.Register("recorder.Analyst", key, constructor)
+}
+
+func NewAnalystWithOptions(options *refx.TypeOptions, opts ...refx.Option) (Analyst, error) {
+	if options.Namespace == "" {
+		options.Namespace = "recorder.Analyst"
+	}
+	v, err := refx.NewType(reflect.TypeOf((*Analyst)(nil)).Elem(), options, opts...)
+	if err != nil {
+		return nil, errors.WithMessage(err, "refx.NewType failed")
+	}
+
+	return v.(Analyst), nil
+}
+
+type Analyst interface {
+	TimeRange() (time.Time, time.Time, error)
+	Stat() StatStream
+}
+
+type StatStream interface {
+	HasNext() bool
+	Next() *UnitStat
+}
---
+++ internal/recorder/analyst_file.go
@@ -0,0 +1,132 @@
+package recorder
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"time"
+
+	jsoniter "github.com/json-iterator/go"
+	"github.com/pkg/errors"
+)
+
+type FileAnalystOptions struct {
+	FilePath string
+}
+
+func NewFileAnalystWithOptions(options *FileAnalystOptions) (*FileAnalyst, error) {
+	return &FileAnalyst{
+		options: options,
+	}, nil
+}
+
+type FileAnalyst struct {
+	options *FileAnalystOptions
+}
+
+func (r *FileAnalyst) TimeRange() (time.Time, time.Time, error) {
+	const kBufSize = 4096
+
+	fp, err := os.Open(r.options.FilePath)
+	if err != nil {
+		return time.Time{}, time.Time{}, errors.WithMessage(err, "os.Open failed")
+	}
+	defer fp.Close()
+
+	var head bytes.Buffer
+	// head -1
+	{
+		buf := make([]byte, kBufSize)
+	headOut:
+		for {
+			n, err := fp.Read(buf)
+			if err != nil && err != io.EOF {
+				return time.Time{}, time.Time{}, errors.WithMessage(err, "os.Open failed")
+			}
+			if err == io.EOF {
+				break
+			}
+
+			for i := 0; i < n; i++ {
+				if buf[i] == '\n' {
+					head.Write(buf[0:i])
+					break headOut
+				}
+			}
+			head.Write(buf[0:n])
+		}
+	}
+
+	var tail bytes.Buffer
+	// tail -1
+	{
+		var rtail bytes.Buffer
+		buf := make([]byte, kBufSize)
+
+		// 获取文件大小
+		offset, err := fp.Seek(0, io.SeekEnd)
+		if err != nil {
+			return time.Time{}, time.Time{}, errors.Wrap(err, "fp.Seek failed")
+		}
+
+	tailOut:
+		for i := 0; ; i++ {
+			n := 0 // 将要读取的字节数
+			if offset < kBufSize {
+				offset = 0
+				n = int(offset)
+			} else {
+				offset -= kBufSize
+				n = kBufSize
+			}
+
+			_, err := fp.Seek(offset, io.SeekStart)
+			if err != nil {
+				return time.Time{}, time.Time{}, errors.Wrap(err, "fp.Seek failed")
+			}
+
+			n, err = fp.Read(buf)
+			if err != nil && err != io.EOF {
+				return time.Time{}, time.Time{}, errors.WithMessage(err, "os.Open failed")
+			}
+
+			for j := n - 1; j >= 0; j-- {
+				if buf[j] == '\n' && (i != 0 || j != n-1) {
+					break tailOut
+				}
+				rtail.WriteByte(buf[j])
+			}
+
+			if offset == 0 {
+				break
+			}
+		}
+
+		for i := rtail.Len() - 1; i >= 0; i-- {
+			tail.WriteByte(rtail.Bytes()[i])
+		}
+	}
+
+	var su UnitStat
+	if err := jsoniter.Unmarshal(head.Bytes(), &su); err != nil {
+		return time.Time{}, time.Time{}, errors.Wrap(err, "jsoniter.Unmarshal failed")
+	}
+	st, err := time.Parse(time.RFC3339Nano, su.Time)
+	if err != nil {
+		return time.Time{}, time.Time{}, errors.Wrap(err, "time.Parse failed")
+	}
+
+	var eu UnitStat
+	if err := jsoniter.Unmarshal(tail.Bytes(), &eu); err != nil {
+		return time.Time{}, time.Time{}, errors.Wrap(err, "jsoniter.Unmarshal failed")
+	}
+	et, err := time.Parse(time.RFC3339Nano, eu.Time)
+	if err != nil {
+		return time.Time{}, time.Time{}, errors.Wrap(err, "time.Parse failed")
+	}
+
+	return st, et, nil
+}
+
+type FileRecorderStatStream struct {
+}
