55655449e168d9ef1c59c791cdc662e2047a32b2
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2022-12-18 01:32:37 +0800

    update

--- internal/monitor/monitor_acm.go
+++ internal/monitor/monitor_acm.go
@@ -8,7 +8,9 @@ import (
 	openapiutil "github.com/alibabacloud-go/openapi-util/service"
 	util "github.com/alibabacloud-go/tea-utils/v2/service"
 	"github.com/alibabacloud-go/tea/tea"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/pkg/errors"
+	"github.com/spf13/cast"
 
 	"github.com/hatlonely/benv2/internal/recorder"
 )
@@ -70,7 +72,7 @@ type MeasurementReq struct {
 }
 
 func (m *ACMMonitor) CollectOnce(req *MeasurementReq) ([]*recorder.Measurement, error) {
-	params := &openapi.Params{
+	cursorParams := &openapi.Params{
 		Action:      tea.String("Cursor"),
 		Version:     tea.String("2021-11-01"),
 		Protocol:    tea.String("HTTPS"),
@@ -82,12 +84,12 @@ func (m *ACMMonitor) CollectOnce(req *MeasurementReq) ([]*recorder.Measurement, 
 		BodyType:    tea.String("json"),
 	}
 
-	body := map[string]interface{}{}
-	body["Namespace"] = tea.String(req.Namespace)
-	body["Metric"] = tea.String(req.Metric)
-	body["Period"] = tea.Int(int(req.Period / time.Second))
-	body["StartTime"] = tea.Int(int(req.StartTime.Unix() * 1000))
-	body["EndTime"] = tea.Int(int(req.EndTime.Unix() * 1000))
+	cursorBody := map[string]interface{}{}
+	cursorBody["Namespace"] = tea.String(req.Namespace)
+	cursorBody["Metric"] = tea.String(req.Metric)
+	cursorBody["Period"] = tea.Int(int(req.Period / time.Second))
+	cursorBody["StartTime"] = tea.Int(int(req.StartTime.Unix() * 1000))
+	cursorBody["EndTime"] = tea.Int(int(req.EndTime.Unix() * 1000))
 	var matchers []map[string]*string
 	for _, matcher := range req.Matchers {
 		matchers = append(matchers, map[string]*string{
@@ -96,20 +98,100 @@ func (m *ACMMonitor) CollectOnce(req *MeasurementReq) ([]*recorder.Measurement, 
 			"Value":    tea.String(matcher.Value),
 		})
 	}
-	body["Matchers"] = openapiutil.ArrayToStringWithSpecifiedStyle(matchers, tea.String("Matchers"), tea.String("json"))
-	runtime := &util.RuntimeOptions{}
-	request := &openapi.OpenApiRequest{
-		Body: body,
-	}
+	cursorBody["Matchers"] = openapiutil.ArrayToStringWithSpecifiedStyle(matchers, tea.String("Matchers"), tea.String("json"))
 
-	res, err := m.client.CallApi(params, request, runtime)
+	cursorResV, err := m.client.CallApi(cursorParams, &openapi.OpenApiRequest{
+		Body: cursorBody,
+	}, &util.RuntimeOptions{})
 	if err != nil {
 		return nil, errors.Wrap(err, "m.client.CallApi failed")
 	}
 
-	fmt.Println(res)
+	var cursorRes struct {
+		Success bool
+		Code    int
+		Data    struct {
+			Cursor string
+		}
+		Message   string
+		RequestId string
+	}
 
-	return nil, nil
+	buf, _ := jsoniter.Marshal(cursorResV["body"])
+	_ = jsoniter.Unmarshal(buf, &cursorRes)
+
+	// batchGet
+	batchGetParams := &openapi.Params{
+		Action:      tea.String("BatchGet"),
+		Version:     tea.String("2021-11-01"),
+		Protocol:    tea.String("HTTPS"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		Pathname:    tea.String("/"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	batchGetBody := map[string]interface{}{}
+	batchGetBody["Namespace"] = tea.String(req.Namespace)
+	batchGetBody["Metric"] = tea.String(req.Metric)
+	batchGetBody["Cursor"] = tea.String(cursorRes.Data.Cursor)
+	batchGetBody["Length"] = tea.Int32(100000)
+
+	batchGetResV, err := m.client.CallApi(batchGetParams, &openapi.OpenApiRequest{
+		Body: batchGetBody,
+	}, &util.RuntimeOptions{})
+	if err != nil {
+		return nil, errors.Wrap(err, "m.client.CallApi failed")
+	}
+
+	var batchGetRes struct {
+		Success bool
+		Code    int
+		Data    struct {
+			Cursor  string
+			Length  int
+			Records []struct {
+				Namespace     string
+				Metric        string
+				MeasureLabels []string
+				MeasureValues []string
+				Labels        []string
+				LabelValues   []string
+				Tags          []string
+				TagValues     []string
+				Timestamp     int
+				Period        int
+			}
+		}
+		Message   string
+		RequestId string
+	}
+
+	buf, _ = jsoniter.Marshal(batchGetResV["body"])
+	_ = jsoniter.Unmarshal(buf, &batchGetRes)
+	//fmt.Println(batchGetResV["body"])
+	//fmt.Println(batchGetRes.Data.Records)
+
+	var measurements []*recorder.Measurement
+	for _, record := range batchGetRes.Data.Records {
+		kvs := map[string]float64{}
+		for idx, key := range record.MeasureLabels {
+			kvs[key] = cast.ToFloat64(record.MeasureValues[idx])
+		}
+
+		val, ok := kvs["Average"]
+		if !ok {
+			val = cast.ToFloat64(record.MeasureValues[0])
+		}
+
+		measurements = append(measurements, &recorder.Measurement{
+			Time:  time.Unix(int64(record.Timestamp/1000), 0),
+			Value: val,
+		})
+	}
+
+	return measurements, nil
 }
 
 func MakeParams() *openapi.Params {
