a01dc6ebfac85b837763ee8d66daf4e19559de8a
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2022-12-18 19:15:58 +0800

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,65 @@
 package main
 
 import (
-	"fmt"
+	"os"
+
+	"github.com/hatlonely/go-kit/config"
+	"github.com/hatlonely/go-kit/flag"
+	"github.com/hatlonely/go-kit/refx"
+	"github.com/hatlonely/go-kit/strx"
+
+	"github.com/hatlonely/benv2/internal/framework"
 )
 
-func main()  {
-	fmt.Println("hello world")
+var Version string
+
+type Options struct {
+	Help     bool   `flag:"-h; usage: show help info"`
+	Version  bool   `flag:"-v; usage: show version"`
+	Action   string `flag:"-a; usage: actions, one of [dep/run/cmd/env/desc/listEnv/listTask]"`
+	Playbook string `flag:"usage: playbook file; default: ben.yaml"`
+}
+
+const (
+	ECSuccess                = 0
+	ECInvalidPlaybook        = 1
+	ECUnmarshalOptionsFailed = 2
+	ECFrameworkNewFailed     = 3
+	ECFrameworkRunFailed     = 4
+)
+
+func main() {
+	var options Options
+	refx.Must(flag.Struct(&options, refx.WithCamelName()))
+	if options.Help {
+		strx.Trac(flag.Usage())
+		strx.Trac(`
+  ben -a run --playbook ben.yaml
+`)
+		return
+	}
+
+	cfg, err := config.NewConfigWithSimpleFile(options.Playbook, config.WithSimpleFileType("Yaml"))
+	if err != nil {
+		strx.Warn(err.Error())
+		os.Exit(ECInvalidPlaybook)
+	}
+
+	var frameworkOptions framework.Options
+	if err := cfg.Unmarshal(&frameworkOptions, refx.WithCamelName()); err != nil {
+		strx.Warn(err.Error())
+		os.Exit(ECUnmarshalOptionsFailed)
+	}
+
+	fw, err := framework.NewFrameworkWithOptions(&frameworkOptions, refx.WithCamelName())
+	if err != nil {
+		strx.Warn(err.Error())
+		os.Exit(ECFrameworkNewFailed)
+	}
+	if err := fw.Run(); err != nil {
+		strx.Warn(err.Error())
+		os.Exit(ECFrameworkRunFailed)
+	}
+
+	os.Exit(ECSuccess)
 }
--- go.mod
+++ go.mod
@@ -4,14 +4,15 @@ go 1.16
 
 require (
 	github.com/PaesslerAG/gval v1.1.0
-	github.com/alibabacloud-go/cms-export-20211101/v2 v2.0.0 // indirect
 	github.com/alibabacloud-go/darabonba-openapi/v2 v2.0.0
+	github.com/alibabacloud-go/openapi-util v0.0.11
+	github.com/alibabacloud-go/tea v1.1.19
+	github.com/alibabacloud-go/tea-utils/v2 v2.0.0
 	github.com/barkimedes/go-deepcopy v0.0.0-20220514131651-17c30cfc62df
 	github.com/generikvault/gvalstrings v0.0.0-20180926130504-471f38f0112a
 	github.com/hatlonely/go-kit v1.1.6
 	github.com/json-iterator/go v1.1.10
 	github.com/pkg/errors v0.9.1
-	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/smartystreets/goconvey v1.7.2
 	github.com/spf13/cast v1.3.1
 )
